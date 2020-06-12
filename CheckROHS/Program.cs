11cd9abdbffe1ccfed316c823afcb441e628f4a4
cli <cli@DE-D-CLI>
2020-06-12 10:51:23 -0700

    Update Program.cs

--- CheckROHS/Program.cs
+++ CheckROHS/Program.cs
@@ -16,7 +16,7 @@ namespace CheckROHS
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new CheckRoHS());
         }
     }
 }
