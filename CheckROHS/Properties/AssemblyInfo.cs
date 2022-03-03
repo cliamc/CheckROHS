12fe21485f29bc99486f180351614f3c7e382c3e
cli <cli@DE-D-CLI>
2022-03-03 15:56:14 -0800

    Update AssemblyInfo.cs

--- CheckROHS/Properties/AssemblyInfo.cs
+++ CheckROHS/Properties/AssemblyInfo.cs
@@ -32,5 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.3")]
 [assembly: AssemblyFileVersion("1.0.0.3")]
+//[assembly: AssemblyVersion("1.0.0.3")]
+[assembly: AssemblyVersion("1.0.0.4")]                          // Use Epicor table FlattenedBOM to check if a part is a sub or not; 11/3/2021
